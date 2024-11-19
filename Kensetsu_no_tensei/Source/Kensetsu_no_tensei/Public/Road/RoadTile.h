0eb8d02cb7213ad794333d4abb0dad0a0d0c9e74
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2024-11-18 20:09:22 -0500

    Add Road assets

--- Kensetsu_no_tensei/Source/Kensetsu_no_tensei/Public/Road/RoadTile.h
+++ Kensetsu_no_tensei/Source/Kensetsu_no_tensei/Public/Road/RoadTile.h
@@ -30,22 +30,7 @@ public:
 	void SetColor(const FLinearColor& NewColor);
 
 	UPROPERTY(EditAnywhere, Category = "Road Mesh");
-	TObjectPtr<class UStaticMesh> RoadMesh0;
-
-	UPROPERTY(EditAnywhere, Category = "Road Mesh");
-	TObjectPtr<class UStaticMesh> RoadMesh1;
-
-	UPROPERTY(EditAnywhere, Category = "Road Mesh")
-	TObjectPtr<class UStaticMesh> RoadMesh2Line;
-
-	UPROPERTY(EditAnywhere, Category = "Road Mesh")
-	TObjectPtr<class UStaticMesh> RoadMesh2Curv;
-
-	UPROPERTY(EditAnywhere, Category = "Road Mesh")
-	TObjectPtr<class UStaticMesh> RoadMesh3;
-
-	UPROPERTY(EditAnywhere, Category = "Road Mesh")
-	TObjectPtr<class UStaticMesh> RoadMesh4;
+	TObjectPtr<class UStaticMesh> RoadMeshes[16];
 
 	void SetMesh(int32 BitCount);
 
